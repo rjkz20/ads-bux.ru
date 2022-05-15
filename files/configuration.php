30a0235533940d9ea11d9bcd3eafd504ca6daa4a
rjkz20 <96689755+rjkz20@users.noreply.github.com>
2022-05-15 21:18:39 +0300

    Create configuration.php

---
+++ files/configuration.php
@@ -0,0 +1,18 @@
+<?php
+
+define('SITE_DOMAIN', 'bymas.ru');
+define('SITE_URL', 'https://' . SITE_DOMAIN . '/');
+
+define('API_KEY', '1281ba0d-593b-4f3e-8b14-2b63e66ae00a');
+
+define('TITLE', 'ByMas');
+define('TITLE_FOOTER', '© Bymas');
+define('FAVICON', 'favicon.ico');
+$META_TAG = [
+ 'DEFAULT' => [
+        'TITLE' => 'KinoPlay',
+        'DESCRIPTION' => 'KinoPlay'
+    ]
+];
+
+?>
