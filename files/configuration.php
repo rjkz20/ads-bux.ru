0610d0d83883b77fbb25cfe34282a72afe3a86bd
rjkz20 <96689755+rjkz20@users.noreply.github.com>
2022-05-15 21:24:59 +0300

    Update configuration.php

--- files/configuration.php
+++ files/configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-define('SITE_DOMAIN', 'bymas.ru');
+define('SITE_DOMAIN', 'rjkz20.github.io/ads-bux.ru');
 define('SITE_URL', 'https://' . SITE_DOMAIN . '/');
 
 define('API_KEY', '1281ba0d-593b-4f3e-8b14-2b63e66ae00a');
